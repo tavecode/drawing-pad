cb2a34ccb8075ea300315d2bd81cb3da66e00206
Tave <131380974+tavecode@users.noreply.github.com>
2023-05-17 06:36:47 -0600

    Create drawing board

--- index.js
+++ index.js
@@ -2,3 +2,30 @@
 // hover/drag effect
 
 
+
+function fillBoard(dimensions) {
+    let container = document.querySelector('.container')
+    container.style.gridTemplateColumns = `repeat(${dimensions} , 1fr)`; //change '16' to variable
+    container.style.gridTemplateRows = `repeat(${dimensions} , 1fr)`;
+    
+    for (let i = 0; i<256; i++) {
+        let square = document.createElement('div')
+//          square.style.backgroundColor = 'blue'
+        container.insertAdjacentElement('beforeend', square);
+    }
+}
+
+
+
+fillBoard(16)
+
+
+
+//size() {
+//    grid = 16;
+//    prompt('Enter grid width (max: 16)') //change to text input box
+//    return grid
+//}
+
+//document.querySelector(gridSize).textContent = `${grid} x ${grid}`
+
