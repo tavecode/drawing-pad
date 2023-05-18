7163a54b64840812323c0320c877d040ab3536b1
Tave <131380974+tavecode@users.noreply.github.com>
2023-05-18 09:23:47 -0600

    Fix dragging bug

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // add 16x16 square divs to container in a grid shape (borders/margins/padding?)
 // hover/drag effect
+
+
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
